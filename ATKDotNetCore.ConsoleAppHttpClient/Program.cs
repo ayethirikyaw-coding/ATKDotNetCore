fd61c8da5fbddbcd491d71ea538a9c5e6a1165fd
AyeThiriKyaw-22 <ayethirikyaw2222@gmail.com>
2024-05-10 15:04:50 +0630

    [Complete] LatHtaukBayDin RestApi

---
+++ ATKDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string jsonStr = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MainDto>(jsonStr)!;
+//Console.WriteLine(jsonStr);
+
+foreach (var q in model.questions)
+{
+    Console.Write(q.questionNo);
+}
+
+static void ToNumber(string num)
+{
+    num.Replace("၃", "3");
+    num.Replace("၁၀", "10");
+}
+
+// JSON to C# ??? package
+// C# to JSON
+
+Console.ReadKey();
+
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
+
