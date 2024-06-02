014e685f95232facbc7a4bac0c7deaa0c563503b
AyeThiriKyaw-22 <ayethirikyaw2222@gmail.com>
2024-06-02 13:22:43 +0630

    [Complete] NLayer Architecture

---
+++ ATKDotNetCore.NLayer.DataAccess/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ATKDotNetCore.NLayer.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATKDotNetCore.NLayer.DataAccess.Db;
+
+internal class AppDbContext : DbContext
+{
+    //Configure database connection
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+    }
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ ATKDotNetCore.NLayer.UI/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ATKDotNetCore.NLayer.BusinessLogic.Services;
+
+Console.WriteLine("Hello, World!");
+
+BL_Blog blBlog = new BL_Blog();
+var lst = blBlog.GetBlogs();
+foreach (var item in lst)
+{
+    Console.WriteLine(item.BlogId);
+    Console.WriteLine(item.BlogTitle);
+    Console.WriteLine(item.BlogAuthor);
+    Console.WriteLine(item.BlogContent);
+    Console.WriteLine("---------------------------------------------");
+}
+
+
