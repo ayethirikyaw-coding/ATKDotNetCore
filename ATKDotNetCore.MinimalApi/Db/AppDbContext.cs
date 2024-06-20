efb087da6a76674081a5efb0ca9ecb24e9edcaf4
AyeThiriKyaw-22 <ayethirikyaw2222@gmail.com>
2024-06-20 20:23:41 +0630

    [Complete] Minimal API CRUD

---
+++ ATKDotNetCore.MinimalApi/Db/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ATKDotNetCore.MinimalApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATKDotNetCore.MinimalApi.Db;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    //Configure database connection
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+    //}
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ ATKDotNetCore.MinimalApi/Models/BlogModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ATKDotNetCore.MinimalApi.Models;
+
+[Table("tbl_Blog")]
+public class BlogModel
+{
+    [Key]
+    public int BlogId { get; set; }
+
+    public string? BlogTitle { get; set; }
+
+    public string? BlogAuthor { get; set; }
+
+    public string? BlogContent { get; set; }
+}
