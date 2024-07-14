ad4d331fbfee55b2a1e009d53022b9f9415b4c99
ayethirikyaw-coding <ayethirikyaw2222@gmail.com>
2024-07-14 17:06:46 +0630

    [Complete] ASP.NET Core MVC Realtime Chat using SignalR

---
+++ ATKDotNetCore.RealTimeChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ATKDotNetCore.RealTimeChatApp.Hubs;
+
+public class ChatHub : Hub
+{
+	//SendMessage
+	public async Task ServerReceiveMessage(string user, string message)
+	{
+		await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+	}
+}
