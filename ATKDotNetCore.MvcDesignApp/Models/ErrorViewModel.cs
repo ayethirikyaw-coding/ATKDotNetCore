700d87993649fb20db21941b4b746c60b259e777
ayethirikyaw-coding <ayethirikyaw2222@gmail.com>
2024-08-16 18:45:22 +0630

    [Complete] ASP.NET Core MVC + UI Design (Admin LTE)

---
+++ ATKDotNetCore.MvcDesignApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATKDotNetCore.MvcDesignApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
