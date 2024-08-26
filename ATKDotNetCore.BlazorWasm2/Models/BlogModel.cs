9b86b2e29c58ae3ff7c82ce4e0051cef39fd9236
ayethirikyaw-coding <ayethirikyaw2222@gmail.com>
2024-08-27 04:51:18 +0630

    feat: ASP.NET Core Blazor Web Assembly with API (GET, POST)

---
+++ ATKDotNetCore.BlazorWasm2/Models/BlogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ATKDotNetCore.BlazorWasm2.Models;
+public class BlogModel
+{
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+}
