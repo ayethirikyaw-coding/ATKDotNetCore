beda1a006d20d2bc1906a3313849603c656d48e6
ayethirikyaw-coding <ayethirikyaw2222@gmail.com>
2024-08-29 00:29:00 +0630

    feat: ASP.NET Core Blazor Server CRUD

---
+++ ATKDotNetCore.BlazorServer/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ATKDotNetCore.BlazorServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATKDotNetCore.BlazorServer.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ ATKDotNetCore.BlazorServer/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ATKDotNetCore.BlazorServer.Data;
+using Microsoft.EntityFrameworkCore;
+using ATKDotNetCore.BlazorServer.Database;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient,
+ServiceLifetime.Transient);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
